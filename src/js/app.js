570fe3ad5b18cc6f6cf7adc343cf016e3a4a3d7c
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2019-04-23 11:37:28 +0200

    Metronome functionality.

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,34 @@ import { pause as pauseTimer, setup as setupTimer, start as startTimer } from '.
 
 const toggleEl = document.getElementById('run-toggle');
 const counterEl = document.getElementById('counter');
+const sliderEl = document.getElementById('slider');
+const decreaseEl = document.getElementById('decrease');
+const increaseEl = document.getElementById('increase');
+const bpmEl = document.getElementById('bpm');
+const min = 40;
+const max = 208;
 
 let audioCtx = null;
 let isRunning = false;
 let isSetup = false;
 let bpm = 120;
 let secondsPerBeat = 60 / bpm;
+
+decreaseEl.addEventListener('click', e => {
+  setBPM(bpm - 1);
+});
+
+increaseEl.addEventListener('click', e => {
+  setBPM(bpm + 1);
+});
+
+sliderEl.addEventListener('input', e => {
+  setBPM(e.target.value);
+});
+
+bpmEl.addEventListener('change', e => {
+  setBPM(e.target.value);
+});
 
 toggleEl.addEventListener('change', e => {
   isRunning = e.target.checked;
@@ -22,6 +44,13 @@ toggleEl.addEventListener('change', e => {
     pauseTimer();
   }
 });
+
+function setBPM(newValue) {
+  bpm = Math.max(min, Math.min(newValue, max));
+  secondsPerBeat = 60 / bpm;
+  bpmEl.value = bpm;
+  sliderEl.value = bpm;
+}
 
 function scheduleEvents(now, scanStart, scanEnd) {
   const firstBeat = Math.ceil(scanStart / secondsPerBeat);
@@ -48,3 +77,5 @@ function performEvent(index, now) {
       counterEl.textContent = index;
     }, delay * 1000);
 }
+
+setBPM(120);
